e546d520924dd4861950f79707dfc5eb468e96b8
Victor Yu <vcyu08@gmail.com>
2017-02-11 13:28:14 -0500

    Create basic web pages and navigations

---
+++ schools/app/controllers/home_controller.rb
@@ -0,0 +1,19 @@
+class HomeController < ApplicationController
+  def index
+  end
+  
+  def about
+  end
+  
+  def learn_school_choice_process
+  end
+
+  def navigate_school_choice
+  end
+
+  def find_feeder_pattern
+  end
+
+  def find_school
+  end
+end
--- schools/config/routes.rb
+++ schools/config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  
+  get 'home/about'
+  
+  get 'home/learn_school_choice_process'
+
+  get 'home/navigate_school_choice'
+
+  get 'home/find_feeder_pattern'
+
+  get 'home/find_school'
+
   get 'schools/index'
 
-  root 'schools#index'
+  root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
